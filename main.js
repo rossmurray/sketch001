47948d2e3c2e7036819265017461bf74da6dda22
rossmurray <rm@egoorb.com>
2017-11-03 02:34:22 -0500

    animate a line

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function update(deltaMs, state) {
     let graphics = state.graphics;
     graphics.clear();
-    //update state (and draw on graphics if using them)
+    graphics.lineStyle(2, 0xFF0000);
+    graphics.moveTo(state.line.startX, state.line.startY);
+    graphics.lineTo(state.line.endX, state.line.endY);
+}
+
+function cardinals() {
+    let cardinals = [];
+    return cardinals;
 }
 
 (function() {
@@ -19,11 +26,34 @@ function update(deltaMs, state) {
     graphics.clear();
     app.stage.addChild(graphics);
 
-    //init things here
+    let line = {
+        startX: 50,
+        startY: 50,
+        endX: 50,
+        endY: 50,
+    };
+    anime({
+        targets: line,
+        endX: 200,
+        endY: 100,
+        duration: 500,
+        easing: 'easeOutSine'
+    });
+    anime({
+        targets: line,
+        startX: 200,
+        startY: 100,
+        duration: 500,
+        easing: 'easeOutSine',
+        delay: 200,
+    });
+    //let cardinals = createCardinals();
+    //let diagonals = [];
     
     let state = {
         app: app,
         graphics: graphics,
+        line: line
     };
     app.ticker.add(function(delta){
         update(delta, state);
